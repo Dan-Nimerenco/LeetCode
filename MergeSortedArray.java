d28f5271522c8cddd6f0d51485b6ed1fc38d9e14
Dan-Nimerenco <nimerencodan@yahoo.com>
2023-02-01 21:04:45 +0200

    Update1

---
+++ MergeSortedArray.java
@@ -0,0 +1,17 @@
+package LeetCode.LeetCode;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+    
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int cnt=0;
+        for (int i=0;i<=nums1.length-1;i++){
+            if (i>(m-1)){
+                nums1[i]=nums2[cnt];
+                cnt++;
+            }
+        }
+        Arrays.sort(nums1);
+    }
+}
