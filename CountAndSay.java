eaaa5b6eb24845e9c6141c906046b9453c543b33
Dan-Nimerenco <123996179+Dan-Nimerenco@users.noreply.github.com>
2023-02-01 11:29:28 +0200

    Add files via upload

---
+++ CountAndSay.java
@@ -0,0 +1,44 @@
+package LeetCode;
+
+public class CountAndSay {
+
+    public static void main(String[] args){
+        System.out.println(countAndSay(5));
+    }
+
+    private static String convTool(String numaru){
+        if (numaru.equals("1")){
+            return "1";
+        }
+        char[] lista = numaru.toCharArray();
+        int cnt=1;
+        String str="";
+        for (int i=0;i<=lista.length-2;i++){
+            if (lista[i]==lista[i+1]){
+                cnt++;
+            }
+            else {
+                str+=cnt+(lista[i]+"");
+                cnt=1;
+            }
+        }
+        str+=cnt+ (lista[lista.length-1]+"");
+        return str;
+    }
+
+
+    public static String countAndSay(int n) {
+        if(n==1){
+            return "1";
+        }
+        String rez;
+        String ver="11";
+        int cnt=3;
+        while(cnt<=n){
+            rez=convTool(ver);
+            ver=rez;
+            cnt++;
+        }
+        return ver;
+    }
+}
